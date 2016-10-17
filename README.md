7c8bdf1f0d5d754a3e96e5d92da132617a15d207
Xiaofan Wu <Xiaofan2406@gmail.com>
2016-10-17 11:28:47 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# VueVue
+Using `vue` as data layer.
+
+
+# VueVue store
+Just an instance of `Vue`.
+
+- data as state
+- methods as actions
+- computed as selectors (getters)
+
+
+# Usage
+
+```js
+// tell Vue to use VueVue plugin
+Vue.use(VueVue);
+
+// create a VueVue store
+const store = {
+  counter: new Vue({
+    data() {
+      return {
+        count: 0
+      };
+    },
+    methods: {
+      plus() {
+        this.count++;
+      },
+      minus() {
+        this.count--;
+      }
+    }
+  })
+};
+
+// tell root Vue instance to use the store
+new Vue({
+  el: '#app',
+  store
+});
+
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+function install(_Vue) {
+  _Vue.mixin({
+    beforeCreate() {
+      const options = this.$options;
+        // store injection
+      if (options.store) {
+        this.$store = options.store;
+      } else if (options.parent && options.parent.$store) {
+        this.$store = options.parent.$store;
+      }
+    }
+  });
+}
+
+
+function isValidStore(store) {
+  // TODO disallow some properties
+  return Boolean(store.$data);
+}
+
+
+function createStore(substores) {
+  const rootStore = {};
+  const names = Object.keys(substores);
+  for (const name of names) {
+    if (isValidStore(substores[name])) {
+      rootStore[name] = substores[name];
+    }
+  }
+  return rootStore;
+}
+
+
+export default {
+  createStore,
+  install
+};
